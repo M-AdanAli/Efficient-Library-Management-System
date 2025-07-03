206e6ebd32f704084d93ad465749f056445f5830
M-AdanAli <imadanirfan@gmail.com>
2025-07-03 09:04:12 +0500

    Added implementation to 'User' class

--- src/com/adanali/library/model/User.java
+++ src/com/adanali/library/model/User.java
@@ -1,4 +1,44 @@
 package com.adanali.library.model;
 
 public abstract class User {
+
+    String name;
+    String userId;
+    String userPassword;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isEmpty()){
+            System.err.println("Name cannot be empty!");
+        }else{
+            this.name = name;
+        }
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        if (userId == null || userId.isEmpty()){
+            System.err.println("User Id cannot be empty!");
+        }else {
+            this.userId = userId;
+        }
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        if (userPassword == null || userPassword.isEmpty() || !userPassword.matches("\\d+")){
+            System.err.println("Not a valid Password");
+        }else {
+            this.userPassword = userPassword;
+        }
+    }
 }
