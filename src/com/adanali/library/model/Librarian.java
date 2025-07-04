c4cee834b624e3eaed123ab51034bb41d347b1f8
M-AdanAli <imadanirfan@gmail.com>
2025-07-04 22:29:13 +0500

    added implementation to 'Librarian' class

--- src/com/adanali/library/model/Librarian.java
+++ src/com/adanali/library/model/Librarian.java
@@ -1,4 +1,18 @@
 package com.adanali.library.model;
 
-public class Librarian {
+/**
+ * Represents a librarian user in the library system.
+ */
+public class Librarian extends User {
+
+    public Librarian(String name, String userId, String password) {
+        super(name, userId, password);
+    }
+
+    @Override
+    public String getRole() {
+        return "Librarian";
+    }
+
+    // Wil add librarian-specific methods here in the future, if needed.
 }
