5d1ebb1fc08de7a466d1b0d6de496ee2032f367b
M-AdanAli <imadanirfan@gmail.com>
2025-07-16 14:49:43 +0500

    Stringutil class

--- src/com/adanali/library/util/StringUtil.java
+++ src/com/adanali/library/util/StringUtil.java
@@ -2,19 +2,19 @@ package com.adanali.library.util;
 
 public class StringUtil {
 
-    public static boolean isNotNullOrBlank(String str){
-        return (str!=null) && !(str.trim().isEmpty());
+    public static boolean isNotNullOrBlank(String str) {
+        return (str != null) && !(str.trim().isEmpty());
     }
 
-    public static boolean isValidEmail(String email){
-        if (isNotNullOrBlank(email)){
+    public static boolean isValidEmail(String email) {
+        if (isNotNullOrBlank(email)) {
             String emailPattern = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
             return email.matches(emailPattern);
         }
         return false;
     }
 
-    public static boolean isValidPassword(String password){
+    public static boolean isValidPassword(String password) {
         /*
         ^: Asserts the start of the string.
         (?=.*[0-9]): Positive lookahead ensuring at least one digit (0-9) is present.
@@ -26,27 +26,27 @@ public class StringUtil {
         $: Asserts the end of the string.
         */
 
-        if (isNotNullOrBlank(password)){
+        if (isNotNullOrBlank(password)) {
             String passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])(?=\\S+$).{8,20}$";
             return password.matches(passwordPattern);
         }
         return false;
     }
 
-    public static boolean isValidIsbn(String isbn){
-        if (isNotNullOrBlank(isbn)){
+    public static boolean isValidIsbn(String isbn) {
+        if (isNotNullOrBlank(isbn)) {
             // Step 1: Check regex pattern (hyphens + group lengths) OR plain pattern without hyphen
-            boolean hyphenedPatternValid = isbn.matches("^(?:978|979)-(\\d{1,5})-(\\d{1,7})-(\\d{1,6})-\\d$") && (isbn.length() == 17) ;
+            boolean hyphenedPatternValid = isbn.matches("^(?:978|979)-(\\d{1,5})-(\\d{1,7})-(\\d{1,6})-\\d$") && (isbn.length() == 17);
             boolean plainPattern = isbn.matches("^(978|979)\\d{10}$");
             // Step 2: Verify total length = 17 characters
             // (3 prefix + 9 group digits + 1 check digit + 4 hyphens)
-            return (hyphenedPatternValid || plainPattern) ;
+            return (hyphenedPatternValid || plainPattern);
         }
         return false;
     }
 
-    public static boolean isNumber(String value){
-        if (isNotNullOrBlank(value)){
+    public static boolean isNumber(String value) {
+        if (isNotNullOrBlank(value)) {
             return value.matches("\\d+");
         }
         return false;
