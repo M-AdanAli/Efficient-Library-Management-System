d20454aeb0e70f9792a1ea36b4daca9b1c338cea
M-AdanAli <imadanirfan@gmail.com>
2025-07-24 12:23:11 +0500

    Modified 'Book' & 'BookService' classes
    Utalized 'StringUtil' class in both the classes. Implemented equals() and hashCode() method in 'Book' class. Added 'searchBookByTitle' and 'searchBookByAuthor' methods in 'BookService' class.

--- src/com/adanali/library/model/Book.java
+++ src/com/adanali/library/model/Book.java
@@ -1,6 +1,9 @@
 package com.adanali.library.model;
 
+import com.adanali.library.util.StringUtil;
+
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Book {
 
@@ -25,10 +28,10 @@ public class Book {
     }
 
     public void setIsbn(String inputIsbn) {
-        if (inputIsbn == null || inputIsbn.length() != 13 || !inputIsbn.matches("\\d{13}")) {
-            System.err.println("Not a valid ISBN number!");
-        }else {
+        if (StringUtil.isValidIsbn(inputIsbn)) {
             this.isbn = inputIsbn;
+        }else {
+            System.err.println("Not a valid ISBN number!");
         }
 
     }
@@ -38,10 +41,10 @@ public class Book {
     }
 
     public void setTitle(String title) {
-        if (title == null || title.isEmpty()) {
-            System.err.println("Title cannot be empty.");
-        }else{
+        if (StringUtil.isNotNullOrBlank(title)) {
             this.title = title;
+        }else{
+            System.err.println("Title cannot be empty.");
         }
 
     }
@@ -51,10 +54,10 @@ public class Book {
     }
 
     public void setAuthor(String author) {
-        if (author == null || author.isEmpty()) {
-            System.err.println("Author name cannot be empty.");
-        }else{
+        if (StringUtil.isNotNullOrBlank(author)) {
             this.authorName = author;
+        }else{
+            System.err.println("Author name cannot be empty.");
         }
 
     }
@@ -64,10 +67,10 @@ public class Book {
     }
 
     public void setGenre(String genre) {
-        if (genre == null || genre.isEmpty()) {
-            System.err.println("Genre cannot be empty.");
-        }else{
+        if (StringUtil.isNotNullOrBlank(genre)) {
             this.genre = genre;
+        }else{
+            System.err.println("Genre cannot be empty.");
         }
 
     }
@@ -77,10 +80,10 @@ public class Book {
     }
 
     public void setPublicationDate(LocalDate publicationDate) {
-        if (publicationDate == null) {
-            System.err.println("Publication date cannot be null.");
-        }else {
+        if (publicationDate != null) {
             this.publicationDate = publicationDate;
+        }else {
+            System.err.println("Publication date cannot be null.");
         }
 
     }
@@ -90,17 +93,28 @@ public class Book {
     }
 
     public void setQuantity(int quantity) {
-        if (quantity < 0) {
-            System.err.println("Quantity cannot be negative.");
-        }else{
+        if (quantity > 0) {
             this.quantity = quantity;
+        }else{
+            System.err.println("Quantity cannot be zero/negative.");
         }
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Book)) return false;
+        Book book = (Book) o;
+        return Objects.equals(getIsbn(), book.getIsbn()) && Objects.equals(getTitle(), book.getTitle()) && Objects.equals(authorName, book.authorName) && Objects.equals(getGenre(), book.getGenre()) && Objects.equals(getPublicationDate(), book.getPublicationDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getIsbn(), getTitle(), authorName, getGenre(), getPublicationDate());
     }
 
     @Override
     public String toString() {
         return String.format("Book[ISBN=%s, Title=%s, Author=%s, Genre=%s, Published=%s, Quantity=%d]",
-                isbn, title, authorName, genre, publicationDate, quantity);
+                getIsbn(), getTitle(), getAuthor(), getGenre(), getPublicationDate(), getQuantity());
     }
 }
--- src/com/adanali/library/service/BookService.java
+++ src/com/adanali/library/service/BookService.java
@@ -1,6 +1,7 @@
 package com.adanali.library.service;
 
 import com.adanali.library.model.Book;
+import com.adanali.library.util.StringUtil;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -14,7 +15,7 @@ public class BookService {
     }
 
     public void addBook(Book book){
-        if (book != null && getBookByIsbn(book.getIsbn()) == null){
+        if (book != null && searchBookByIsbn(book.getIsbn()) == null){
             books.add(book);
         } else {
             System.err.println("Book already exists or invalid input!");
@@ -22,7 +23,7 @@ public class BookService {
     }
 
     public boolean removeBook(String isbn){
-        if (isbn != null && isbn.length() == 13 && isbn.matches("\\d{13}")){
+        if (StringUtil.isValidIsbn(isbn)){
             return books.removeIf(b -> b.getIsbn().equals(isbn));
         }else {
             System.err.println("Pass a valid ISBN Number!");
@@ -31,7 +32,7 @@ public class BookService {
     }
 
     public boolean updateBook(String isbn, Book updatedBook){
-        if (updatedBook != null && isbn != null && isbn.length() == 13 && isbn.matches("\\d{13}")){
+        if (updatedBook != null && StringUtil.isValidIsbn(isbn)){
             for (int i = 0; i < books.size(); i++) {
                 if (books.get(i).getIsbn().equals(isbn)) {
                     books.set(i, updatedBook);
@@ -44,8 +45,8 @@ public class BookService {
         return false;
     }
 
-    public Book getBookByIsbn(String isbn){
-        if (isbn != null && isbn.length() == 13 && isbn.matches("\\d{13}")){
+    public Book searchBookByIsbn(String isbn){
+        if (StringUtil.isValidIsbn(isbn)){
             for (Book book : books){
                 if (book.getIsbn().equals(isbn)){
                     return book;
@@ -59,7 +60,7 @@ public class BookService {
 
     public List<Book> searchBook(String query){
         List<Book> result = new ArrayList<Book>();
-        if (query != null && !query.isEmpty()){
+        if (StringUtil.isNotNullOrBlank(query)){
             query = query.toLowerCase();
             for (Book book : books){
                 String titleLowercase = book.getTitle().toLowerCase();
@@ -72,7 +73,33 @@ public class BookService {
         }else {
             System.err.println("Pass a valid argument!");
         }
-        return result;
+        return Collections.unmodifiableList(result);
+    }
+
+    public List<Book> searchBookByTitle(String title) {
+        List<Book> result = new ArrayList<>();
+        if (StringUtil.isNotNullOrBlank(title)) {
+            title = title.toLowerCase();
+            for (Book book : books) {
+                if (book.getTitle().toLowerCase().contains(title)) {
+                    result.add(book);
+                }
+            }
+        }
+        return Collections.unmodifiableList(result);
+    }
+
+    public List<Book> searchBookByAuthor(String author) {
+        List<Book> result = new ArrayList<>();
+        if (StringUtil.isNotNullOrBlank(author)) {
+            author = author.toLowerCase();
+            for (Book book : books) {
+                if (book.getAuthor().toLowerCase().contains(author)) {
+                    result.add(book);
+                }
+            }
+        }
+        return Collections.unmodifiableList(result);
     }
 
     public List<Book> listAllBooks(){
@@ -80,7 +107,7 @@ public class BookService {
     }
 
     public boolean isBookAvailable(String isbn){
-            Book b = getBookByIsbn(isbn);
+            Book b = searchBookByIsbn(isbn);
             return b!=null && b.getQuantity()>0;
     }
 }
