0beab4327dc16044b724df72a84aa6361bb302eb
M-AdanAli <imadanirfan@gmail.com>
2025-09-04 17:58:53 +0500

    Changes in UserService in accordance with the UsersRepository

--- src/com/adanali/library/model/Borrower.java
+++ src/com/adanali/library/model/Borrower.java
@@ -2,7 +2,7 @@ package com.adanali.library.model;
 
 public interface Borrower {
     int getPendingFine();
-    void addPendingFine(int fine);
+    boolean addPendingFine(int fine);
     boolean reducePendingFine(int fine);
     default boolean canBorrow(){
         return getPendingFine()==0;
--- src/com/adanali/library/service/UserService.java
+++ src/com/adanali/library/service/UserService.java
@@ -1,103 +1,106 @@
 package com.adanali.library.service;
 
 import com.adanali.library.model.User;
+import com.adanali.library.repository.UsersRepository;
 import com.adanali.library.util.StringUtil;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public class UserService {
-    List<User> users;
+    private UsersRepository usersRepository;
 
     public UserService(){
-        users = new ArrayList<>();
+        usersRepository = new UsersRepository();
     }
 
-    public String authenticate(String email, String password){
+    public User authenticate(String email, String password){
         if (StringUtil.isValidEmail(email)){
-            User user = getUserByEmail(email);
-            if (user != null && user.getPassword().equals(password)){
-                return user.getRole();
+            User user = getUserByEmail(email).orElse(null);
+            if (user != null){
+                if (user.getPassword().equals(password)){
+                    return user;
+                }else{
+                    System.err.println("Invalid Password!");
+                }
+            }else {
+                System.err.println("User does not exists!");
             }
+        }else{
+            System.err.println("Pass a valid E-mail!");
         }
         return null;
     }
 
-    public void addUser(User user){
-        if (user != null && getUserByEmail(user.getEmail()) == null){
-            users.add(user);
+    public boolean addUser(User user){
+        if (user != null){
+            usersRepository.add(user);
+            return true;
         }else {
             System.err.println("Pass a valid user!");
+            return false;
         }
     }
 
     public boolean removeUser(String email){
         if (StringUtil.isValidEmail(email)){
-            return users.removeIf(u -> u.getEmail().equalsIgnoreCase(email));
+            return usersRepository.remove(email);
         }else {
             System.err.println("Pass a valid E-mail!");
+            return false;
         }
-        return false;
+    }
+
+    public Optional<User> getUserByEmail(String email){
+        if (StringUtil.isValidEmail(email)){
+            return usersRepository.getById(email);
+        }
+        return Optional.empty();
+    }
+
+    public List<User> listAllUsers(){
+        return usersRepository.getAll();
+    }
+
+    public List<User> searchUsers(String query){
+        if (StringUtil.isNotNullOrBlank(query)){
+            return usersRepository.search(query);
+        }else {
+            System.err.println("Invalid Arguments!");
+        }
+        return Collections.unmodifiableList(new ArrayList<>(0));
     }
 
     public boolean updateUserName(String email, String newName){
         if (StringUtil.isValidEmail(email) && StringUtil.isNotNullOrBlank(newName)){
-            User user = getUserByEmail(email);
+            User user = getUserByEmail(email).get();
             if (user != null){
                 user.setName(newName);
                 return true;
             }
+            System.err.println("User does not exists!");
+            return false;
         }else {
-            System.err.println("Invalid email or name!");
+            System.err.println("Invalid E-mail or Name!");
+            return false;
         }
-        return false;
     }
 
     public boolean updatePassword(String email, String newPassword){
         if (StringUtil.isValidEmail(email) && StringUtil.isValidPassword(newPassword)){
-            User user = getUserByEmail(email);
+            User user = getUserByEmail(email).get();
             if (user != null){
                 user.setPassword(newPassword);
                 return true;
             }
+            System.err.println("User does not exists!");
+            return false;
         }else {
             System.err.println("Invalid email or new Password!");
+            return false;
         }
-        return false;
-    }
-
-    public User getUserByEmail(String email){
-        if(StringUtil.isValidEmail(email)){
-            for (User user : users){
-                if (user.getEmail().equalsIgnoreCase(email)){
-                    return user;
-                }
-            }
-            System.err.println("User does not exists!");
-        }else {
-            System.err.println("Pass a valid E-mail!");
-        }
-        return null;
-    }
-
-    public List<User> listAllUsers(){
-        return Collections.unmodifiableList(users);
-    }
-
-    public List<User> searchUser(String query){
-        List<User> result = new ArrayList<>();
-        if (StringUtil.isNotNullOrBlank(query)){
-            query = query.toLowerCase();
-            for(User user : users){
-                String emailLowercase = user.getEmail().toLowerCase();
-                String nameLowercase = user.getName().toLowerCase();
-               if (emailLowercase.contains(query) || nameLowercase.contains(query)){
-                   result.add(user);
-               }
-            }
-        }
-        return Collections.unmodifiableList(result);
     }
 
 }
